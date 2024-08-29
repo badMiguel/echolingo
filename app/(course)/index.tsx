31cd0b365361f233766cb391bf51f8cc4b5f9b3d
MiguelBad <badayosbiboy@gmail.com>
2024-08-29 15:40:51 +0930

    display current dharug details

--- app/(course)/index.tsx
+++ app/(course)/index.tsx
@@ -4,19 +4,83 @@ import { StyleSheet, Text, View } from 'react-native'
 import dharugData from '../../data/json/dharug_list.json'
 import courseData from '../../data/json/course_data.json'
 
+type DharugDataType = {
+    id: number;
+    English: string | null;
+    "Gloss (english)": string | null;
+    "Dharug(Gloss)": string | null;
+    Dharug: string | null;
+    Topic: string | null;
+    "Image Name (optional)": any;
+    recording: any;
+    completed: boolean;
+};
+
+type CurrentType = {
+    current: DharugDataType | null;
+}
+
 function Course() {
     const param = useLocalSearchParams();
-    let courseName: string = Array.isArray(param.courseName) ? param.courseName[0] : param.courseName;
-    filterPhrases(courseName);
+    let courseName: string = Array.isArray(param.courseName)
+        ? param.courseName[0] : param.courseName;
+
+    const dharugList = filterDharug(courseName);
+    const current: DharugDataType | null = dharugList.length > 0 ? dharugList[0] : null;
+
     return (
         <View>
+            <Question current={current} />
         </View>
     )
 }
 
-function filterPhrases(courseName: string) {
+function filterDharug(courseName: string): DharugDataType[] {
     const selectedCourse = courseData.filter(course => course.courseName === courseName);
-    const phrases = dharugData.filter(item => selectedCourse.some(course => course.topic.includes(item.Topic)));
+    return dharugData.filter(item =>
+        selectedCourse.some(course => course.topic.includes(item.Topic)) &&
+        !item.completed
+    );
+}
+
+function Question({ current }: CurrentType) {
+    return (
+        <>
+            {!current ? (
+                <View>
+                    <Text>Congratulations! You have completed this course.</Text>
+                </View>
+            ) : (
+                <View>
+                    {current.Dharug &&
+                        <>
+                            <Text>Dharug:</Text>
+                            <Text>{current.Dharug}</Text>
+                        </>
+                    }
+                    {current['Dharug(Gloss)'] &&
+                        <>
+                            <Text>Dharug (Gloss):</Text>
+                            <Text>{current['Dharug(Gloss)']}</Text>
+                        </>
+                    }
+                    {current.English &&
+                        <>
+                            <Text>English:</Text>
+                            <Text>{current.English}</Text>
+                        </>
+                    }
+                    {current['Gloss (english)'] &&
+                        <>
+                            <Text>English (Gloss):</Text>
+                            <Text>{current['Gloss (english)']}</Text>
+                        </>
+                    }
+                </View>
+            )
+            }
+        </>
+    );
 }
 
 export default Course
