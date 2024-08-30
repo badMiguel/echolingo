95aede4a51271500b5b103b53e69086bdb91a556
MiguelBad <badayosbiboy@gmail.com>
2024-08-31 02:15:59 +0930

    WIP: implement expo av audio

--- app/(tabs)/(addRecording)/record.tsx
+++ app/(tabs)/(addRecording)/record.tsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
+import { Audio } from "expo-av";
 
 export default function Record() {
+    const [recording, setRecording] = useState<Audio.Recording>();
+    const [permissionResponse, requestPermission] = Audio.usePermissions();
+
+    async function startRecording() {
+        try {
+            if (permissionResponse?.status !== 'granted') {
+                await requestPermission();
+            }
+
+            await Audio.setAudioModeAsync({
+                allowsRecordingIOS: true,
+                playsInSilentModeIOS: true,
+            });
+
+            const { recording } = await Audio.Recording.createAsync(
+                Audio.RecordingOptionsPresets.HIGH_QUALITY
+            );
+
+            setRecording(recording);
+
+        } catch (err) {
+            console.error('Failed to start recording', err);
+        }
+    }
+
+    async function stopRecording() {
+        setRecording(undefined);
+        await recording?.stopAndUnloadAsync();
+        await Audio.setAudioModeAsync(
+            {
+                allowsRecordingIOS: false,
+            }
+        );
+
+        const uri = recording?.getURI();
+
+    }
+
     return (
         <View>
+            <Button
+                title={recording ? "Stop Recording" : "Start Recording"}
+                onPress={recording ? stopRecording : startRecording}
+            />
         </View>
     );
 }
