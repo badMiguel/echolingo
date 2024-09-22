1e6397d71563a91a5e2d8800efa652f54b85a2cf
MiguelBad <badayosbiboy@gmail.com>
2024-09-22 23:49:57 +0930

    literal search

--- components/search/search.tsx
+++ components/search/search.tsx
@@ -1,14 +1,15 @@
+import { DataType } from "@/contexts/TiwiContext";
 import { View } from "react-native";
 
 type TrieNode = {
     children: Map<string, TrieNode>;
-    id: number;
+    id: string;
 }
 
 function createTrieNode(): TrieNode {
     return {
         children: new Map(),
-        id: 0
+        id: "0"
     }
 }
 
@@ -20,7 +21,7 @@ class Trie {
         this.root = createTrieNode();
     }
 
-    insert(phrase: string, id: number) {
+    insert(phrase: string, id: string) {
         let node = this.root;
 
         for (const char of phrase) {
@@ -34,46 +35,79 @@ class Trie {
         node.id = id;
     }
 
-    prefixOf(phrase: string): number[] {
-        const potential: number[] = [];
-        let noTrie: boolean = false;
+    prefixOf(searchTerm: string, data: DataType): string[] {
+        const potential: string[] = [];
+        let prefix: string = ""
         let node: TrieNode = this.root;
 
-        for (const char of phrase) {
+        for (const char of searchTerm) {
             if (!node.children.has(char)) {
-                noTrie = true;
-                break;
+                return this.literalSearch(searchTerm, data);
             }
 
-            if (node.id !== 0) {
+            prefix += char;
+            if (node.id !== "0") {
                 potential.push(node.id);
             }
 
-            try {
+            if (potential.length > 11) {
+                return potential;
+            }
+
+            if (typeof node === "object") {
                 node = node.children.get(char)!;
-            } catch {
+            } else {
                 break;
             }
         }
 
-        if (noTrie) {
-            return literalSearch();
-        } else {
-            this.checkChild(potential, node);
-            return potential;
+        this.checkChild(potential, prefix, node);
+        return potential;
+    }
+
+    private checkChild(potential: string[], prefix: string, node: TrieNode) {
+        if (potential.length > 11) {
+            return;
         }
 
+        if (node.id !== "0") {
+            potential.push(node.id);
+        }
+
+        for (const child in node.children) {
+            this.checkChild(potential, prefix + child, node.children.get(child)!);
+        }
     }
 
-    private checkChild(potential: number[], node: TrieNode): number[] {
-        return []; // change this
+    // fallback when no results from trie. perform exhaustive search
+    private literalSearch(searchTerm: string, data: DataType): string[] {
+        const filtered: string[] = [];
+        const searchTerms = searchTerm.split(" ");
+
+        for (const key in Object.keys(data)) {
+            const entry = data[key]
+
+            // only checks if there is at least one match then push to potential
+            const hasMatch = (field: string | null): boolean => {
+                if (!field) return false;
+                const term = field.split(" ");
+                return term.some(word => searchTerms.includes(word));
+            }
+
+            if (
+                hasMatch(entry.English) ||
+                hasMatch(entry.Tiwi) ||
+                hasMatch(entry["Gloss (english)"]) ||
+                hasMatch(entry["Gloss (tiwi)"])
+            ) {
+                filtered.push(key)
+            }
+        }
+
+        return filtered;
     }
 }
 
-// fallback when no results from trie
-function literalSearch(): number[] {
-    return []; // change this
-}
 
 export default function SearchBar() {
     return (
