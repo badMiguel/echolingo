3690734889c308edde03de956a4207729911746a
MiguelBad <badayosbiboy@gmail.com>
2024-09-23 20:32:28 +0930

    changed images

--- constants/images.ts
+++ constants/images.ts
@@ -4,11 +4,9 @@ type imageType = {
     [key: string]: string;
 }
 
-// todo make sure image are appropriate
 const images: imageType = {
-    "Feelings and Behaviours": require('../assets/images/health-and-wellbeing.jpg'),
-    "Greetings and Checking In": require('../assets/images/audrey-rubuntja-woman-gathering-bush-tucker-200808311339-1024x1024.jpg'),
-    // "Others": require('../assets/images/Australian-Aborignial-Art-f5d4a953-b130-4e1d-9a36-e0f07a31112c.png'),
+    "Feelings and Behaviours": require('@/assets/images/placeholder.jpg'),
+    "Greetings and Checking In": require('@/assets/images/placeholder.jpg'),
 };
 
 export default images;
