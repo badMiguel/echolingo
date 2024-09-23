7ebeb73d9292a7906428934c747f6551201fd301
KienTrannn <s367251@students.cdu.edu.au>
2024-09-23 20:52:12 +0930

    commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -107,7 +107,6 @@ export default function RootLayout() {
                             href: userType !== 'student' ? null : "/(challenges)"
                         }}
                     />
-
                     <Tabs.Screen
                         name="profile"
                         options={{
