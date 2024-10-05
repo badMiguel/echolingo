36e0aaba58348e7a51e9f7e61aa5c09c6818ac12
MiguelBad <badayosbiboy@gmail.com>
2024-10-05 16:57:22 +0930

    build: firebase setup

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,20 +1,27 @@
 import { initializeApp } from "firebase/app";
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
 import { getFirestore } from "firebase/firestore";
+// import {...} from "firebase/functions";
 import { getStorage } from "firebase/storage";
-import 'firebase/database'
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyDCJw27c6rhm2SeWoc6qSl0drwBX-8AEv8",
-    authDomain: "languageapp-b7daf.firebaseapp.com",
-    databaseURL: "https://languageapp-b7daf.firebaseio.com",
-    projectId: "languageapp-b7daf",
-    storageBucket: "languageapp-b7daf.appspot.com",
-    messagingSenderId: "97596244175",
-    appId: "1:97596244175:android:ed44b28a4a5860c52bf7ba",
+    apiKey: "AIzaSyDoWuDkE2J8gbUAoHvhyjb1U9jkYIMS6lk",
+    authDomain: "echolingo-4aa6d.firebaseapp.com",
+    // databaseURL: "https://languageapp-b7daf.firebaseio.com",
+    projectId: "echolingo-4aa6d",
+    storageBucket: "echolingo-4aa6d.appspot.com",
+    messagingSenderId: "89256999282",
+    appId: "1:89256999282:android:c2b9e225c75e4a612345eb",
+    measurementId: "G-NH58WWKJCD",
 };
 
 const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
 
-export const data = getFirestore(app); // Export Firestore
+export const db = getFirestore(app);
 export const storage = getStorage(app);
