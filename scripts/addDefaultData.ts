80da811478833927e5de0b9991473fa18360f293
MiguelBad <badayosbiboy@gmail.com>
2024-10-05 22:46:48 +0930

    chore: added script to populate sentence list data in firebase

---
+++ scripts/addDefaultData.ts
@@ -0,0 +1,34 @@
+import { db } from "../firebase/firebaseConfig";
+import { collection, addDoc } from "firebase/firestore";
+import data from "../data/json/tiwi_list.json";
+
+// save default data in json to firebase
+// run: ts-node addDefaultData.ts
+async function addDefaultData() {
+    console.log("hi");
+    try {
+        for (const item of data) {
+            const docRef = await addDoc(collection(db, "sentences"), {
+                English: item.English,
+                Tiwi: item.Tiwi,
+                Topic: item.Topic,
+                "Gloss (english)": item["Gloss (english)"],
+                "Gloss (tiwi)": item["Gloss (tiwi)"],
+                "Image name (optional)": item["Image name (optional)"],
+                recording: item.recording,
+                completed: item.completed,
+            });
+
+            console.log(`Added item with ref: ${docRef.id}`);
+        }
+
+        console.log("Successfully added all data");
+    } catch (error) {
+        console.error("Error adding default data:", error);
+
+    }
+
+    return
+}
+
+addDefaultData()
---
+++ scripts/usage.md
@@ -0,0 +1,11 @@
+ensure ts-node is installed
+
+```bash
+npm install ts-node --save-dev
+```
+
+then run:
+
+```bash
+npx ts-node path-to-file.ts
+```
