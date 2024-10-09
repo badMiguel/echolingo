1207b988ce06b9b26985d1a8b908830e9640cc98
miguel <badayosbiboy@gmail.com>
2024-10-09 11:43:29 +0930

    wip: add user context

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,22 @@ import { View, Button, StyleSheet } from "react-native";
 import React, { createContext } from "react";
 import { router } from "expo-router";
 import { useThemeColor } from "@/hooks/useThemeColor";
+import { UserTypeProvider, useSetUserTypeContext } from "@/contexts/UserType";
 
 export const UserTypeContext = createContext("");
 
 export default function Index() {
     const bgColor = useThemeColor({}, "background");
     const buttonColor = useThemeColor({}, "primary");
+    const setUserType = useSetUserTypeContext();
 
     const login = (type: string, name: string) => {
+        // todo better error handling
+        if (!setUserType) {
+            console.error("failed to use setUserTypeContext");
+        } else {
+            setUserType(type);
+        }
         router.push({
             pathname: "/(tabs)",
             params: {
@@ -20,18 +28,20 @@ export default function Index() {
     };
 
     return (
-        <View style={[styles.mainView, { backgroundColor: bgColor }]}>
-            <Button
-                color={buttonColor}
-                onPress={() => login("student", "Student")}
-                title="Student View"
-            />
-            <Button
-                color={buttonColor}
-                onPress={() => login("teacher", "Teacher")}
-                title="Teacher View"
-            />
-        </View>
+        <UserTypeProvider>
+            <View style={[styles.mainView, { backgroundColor: bgColor }]}>
+                <Button
+                    color={buttonColor}
+                    onPress={() => login("student", "Student")}
+                    title="Student View"
+                />
+                <Button
+                    color={buttonColor}
+                    onPress={() => login("teacher", "Teacher")}
+                    title="Teacher View"
+                />
+            </View>
+        </UserTypeProvider>
     );
 }
 
---
+++ contexts/UserType.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode, createContext, useContext, useState } from "react";
+
+const UserTypeContext = createContext<string | undefined>(undefined);
+const SetUserTypeContext = createContext<React.Dispatch<string | undefined> | undefined>(undefined);
+
+export const UserTypeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [userType, setUserType] = useState<string>();
+
+    return (
+        <UserTypeContext.Provider value={userType}>
+            <SetUserTypeContext.Provider value={setUserType}>
+                {children}
+            </SetUserTypeContext.Provider>
+        </UserTypeContext.Provider>
+    );
+};
+
+export function useUserTypeContext() {
+    return useContext(UserTypeContext);
+}
+
+export function useSetUserTypeContext() {
+    const setUserType = useContext(SetUserTypeContext);
+    if (!setUserType) {
+        console.error("Failed to use SetUserTypeContext");
+    }
+
+    return setUserType;
+}
